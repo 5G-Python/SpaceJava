79d978ec101374c7254ca46bd380799afcce98c7
5G-Python <69048058+5G-Python@users.noreply.github.com>
2020-08-06 14:58:06 -0400

    Add files via upload

---
+++ 2Beta4u.py
@@ -0,0 +1,76 @@
+import time
+
+print(
+    "Hello! and welcome to Stick Warfare I: Enemy Terminal! You are currently taking place of Emperor Xiran Green of "
+    "the United Moaft Stickman Republics!")
+time.sleep(1)
+print("Hold on, whats your name?")
+user_name = input()
+if user_name == "Macie":
+    print("MACIE BLYAT!!!")
+    exit()
+if user_name == "Fudgeler":
+    print("FUDGELER BLYAT!!!")
+    exit()
+if user_name == "Redshirt":
+    print('REDSHIRT BLYAT!!!')
+    exit()
+else:
+    if user_name == "Windows":
+        print('Come right on with us!')
+    if user_name == "5G Java":
+        print('This code was made by Enchant for 5G!')
+    if user_name == "Enchant-mint":
+        print("Searching for Enchant-mint...")
+        time.sleep(5)
+        print("Enchant-mint found!")
+
+    print("Anyways, Here's where you are right now.")
+    print("Loading Data inscription...")
+    time.sleep(5)
+    print("Location: Middle Eastern Aranko, in the deep vallies of Fudgeleria and Redshirland")
+    time.sleep(3)
+    print("Alright. Now that you have the details, We will move on.")
+    print("Would you like to attack Fudgeleria now? Yes/Np")
+    answer = input()
+if "Yes" == answer:
+    print("You attack Fudgeleria with over 2.1K Armored Soldiers.")
+    time.sleep(2)
+    print("OH NO!")
+    print("Fudgeleria has called for backup from Redshiland!")
+    print("call in armed forces?")
+    answer = input()
+if answer == "Yes":
+    print("Summoning Codename Plant Number 23...")
+    time.sleep(4)
+    print("Successful Arrival!")
+    print("You are now attacking with your armed forces towards both Fudgeleria and Redshirland.")
+    time.sleep(3)
+    print("After little time, your soldiers managed to clear up half of Fudgeleria and Redshirland!")
+    time.sleep(2)
+    print("OH DEAR LORD!")
+    print("Fudgeleria and Redshirland and calling in Macielandia for help!")
+    time.sleep(1)
+    print("Call Hotland for backup?")
+    answer = input()
+    if answer == "Yes":
+        print("Alright! In a few minutes Hotland will be here!")
+time.sleep(5)
+print("Great! Hotland is here!")
+print("Ready to attack?")
+answer = input()
+if answer == "Yes":
+    print("You send in 2.8K solders.")
+    time.sleep(4)
+print("As it seems, Macielandia has more soldiers than you thought!")
+print("Call Java Commune, Code Cult, and Noezmo?")
+answer = input()
+if answer == "Yes":
+    print("Lets wait for them to arrive!")
+    time.sleep(3)
+    print("What?! Here already?")
+    print("Anyways, Attack Redshirland, Fugdeleria, and Macielanda?")
+    answer = input()
+    if answer == "Yes":
+        print("You send in 4.8K soldiers.")
+    print("END OF BETA PHASE 001")
